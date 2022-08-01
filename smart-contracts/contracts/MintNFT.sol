6a82800aaed3248c2644030c2744d38011d7a6ad
SachinCoder1 <sachinhlo232@gmail.com>
2022-08-01 14:02:07 +0530

    setTokenURI and getTokenURI

--- smart-contracts/contracts/MintNFT.sol
+++ smart-contracts/contracts/MintNFT.sol
@@ -8,6 +8,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+/* Errors */
+error MintNFT__TokenDosentExist();
 
 contract MintNFT is ERC721, Ownable {
     /* State Variables */
@@ -30,4 +32,25 @@ contract MintNFT is ERC721, Ownable {
      constructor() ERC721("Minting NFTs", "Mint"){
         
      }
+
+
+     /* Pure/Get Functions */
+
+     function getTokenURI(uint256 _tokenId) external view returns(string memory){
+        if(!_exists(_tokenId)){
+            revert MintNFT__TokenDosentExist();
+        }
+
+        return s_tokenURIs[_tokenId];
+     }
+
+    
+
+     /* Logics */
+
+     function _setTokenURI(uint256 _tokenId, string memory _tokenURI){
+         s_tokenURIs[_tokenId] = _tokenURI;
+     }
+
+
 }
