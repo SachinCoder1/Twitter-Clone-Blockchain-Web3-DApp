0c7b2056cacaee33ad5c0795a004246cd449f7b1
SachinCoder1 <sachinhlo232@gmail.com>
2022-08-08 11:36:49 +0530

    formatting

--- smart-contracts/contracts/MintNFT.sol
+++ smart-contracts/contracts/MintNFT.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.7;
 
-
 /* Imports  */
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -13,68 +12,67 @@ error MintNFT__TokenDosentExist();
 
 contract MintNFT is ERC721, Ownable {
     /* State Variables */
-       using Counters for Counters.Counter;
+    using Counters for Counters.Counter;
     using Strings for uint256;
 
     Counters.Counter tokenIds;
 
     /* Mappings */
-    mapping (uint256 => string) s_tokenURIs;
+    mapping(uint256 => string) s_tokenURIs;
 
-   /* Structs */
+    /* Structs */
     struct Token {
         uint256 id;
         string uri;
-        string space;        
+        string space;
     }
 
     /* constructor */
-     constructor() ERC721("Minting NFTs", "Mint"){}
+    constructor() ERC721("Minting NFTs", "Mint") {}
 
+    /* Pure/Get Functions */
 
-     /* Pure/Get Functions */
-
-     function getTokenURI(uint256 _tokenId) external view returns(string memory){
-        if(!_exists(_tokenId)){
+    function getTokenURI(uint256 _tokenId)
+        external
+        view
+        returns (string memory)
+    {
+        if (!_exists(_tokenId)) {
             revert MintNFT__TokenDosentExist();
         }
 
         return s_tokenURIs[_tokenId];
-     }
-     
+    }
+
     //  Get all the tokens which exist
-     function getAllTokens() external view returns (Token[] memory) {
+    function getAllTokens() external view returns (Token[] memory) {
         uint256 currentId = tokenIds.current();
         Token[] memory newTokens = new Token[](currentId);
 
         for (uint i = 0; i <= currentId; i++) {
-            if(_exists(i)){
+            if (_exists(i)) {
                 string memory uri = s_tokenURIs[i];
                 newTokens[i] = Token(i, uri, " ");
-
             }
         }
 
         return newTokens;
-     }
+    }
 
-    
+    /* Logics */
 
-     /* Logics */
+    function _setTokenURI(uint256 _tokenId, string memory _tokenURI) private {
+        s_tokenURIs[_tokenId] = _tokenURI;
+    }
 
-     function _setTokenURI(uint256 _tokenId, string memory _tokenURI) private {
-         s_tokenURIs[_tokenId] = _tokenURI;
-     }
-
-     function mintNFT(address _nftAddress, string memory _tokenURI) external returns(uint256) {
+    function mintNFT(address _nftAddress, string memory _tokenURI)
+        external
+        returns (uint256)
+    {
         uint256 currentId = tokenIds.current();
         _mint(_nftAddress, currentId);
-        _setTokenURI(currentId, _tokenURI );
+        _setTokenURI(currentId, _tokenURI);
         tokenIds.increment();
         return currentId;
-
-     }
-     
-
-
+    }
 }
--- twitterweb3/schemas/userTypes.js
+++ twitterweb3/schemas/userTypes.js
@@ -1,43 +1,43 @@
 export const userTypes = {
-    name: 'users',
-    title: 'Users',
-    type: 'document',
-    fields: [
-      {
-        name: 'name',
-        type: 'string',
-        title: 'Name',
-      },
-      {
-        name: 'walletAddress',
-        type: 'string',
-        title: 'Wallet Address',
-      },
-      {
-        name: 'profileImage',
-        type: 'string',
-        title: 'Profile Image',
-      },
-      {
-        name: 'isProfileImageNft',
-        title: 'Is Profile Image NFT',
-        type: 'boolean',
-      },
-      {
-        name: 'coverImage',
-        type: 'string',
-        title: 'Cover Image',
-      },
-      {
-        name: 'tweets',
-        title: 'Tweets',
-        type: 'array',
-        of: [
-          {
-            type: 'reference',
-            to: [{ type: 'tweets' }],
-          },
-        ],
-      },
-    ],
-  }
+  name: "users",
+  title: "Users",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Name",
+    },
+    {
+      name: "walletAddress",
+      type: "string",
+      title: "Wallet Address",
+    },
+    {
+      name: "profileImage",
+      type: "string",
+      title: "Profile Image",
+    },
+    {
+      name: "isProfileImageNft",
+      title: "Is Profile Image NFT",
+      type: "boolean",
+    },
+    {
+      name: "coverImage",
+      type: "string",
+      title: "Cover Image",
+    },
+    {
+      name: "tweets",
+      title: "Tweets",
+      type: "array",
+      of: [
+        {
+          type: "reference",
+          to: [{ type: "tweets" }],
+        },
+      ],
+    },
+  ],
+};
